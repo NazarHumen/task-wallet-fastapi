50c4bea0ab3776a2a2b95922df4b79d177ca056a
NazarHumen <nazargumen172@gmail.com>
2026-07-01 22:03:39 +0300

    feat(config): add JWT settings and fix env_file path

--- src/config.py
+++ src/config.py
@@ -4,8 +4,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     database_url: str
 
+    jwt_secret_key: str
+    jwt_algorithm: str
+    access_token_expire_minutes: int
+
     model_config = SettingsConfigDict(
-        env_file="../.env",
+        env_file=".env",
         env_file_encoding="utf-8",
         case_sensitive=False
     )
