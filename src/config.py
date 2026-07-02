7dd3d705917d7602967ae7ee32a4a9d2bf6203d6
NazarHumen <nazargumen172@gmail.com>
2026-07-02 13:54:37 +0300

    feat(db): add refresh_tokens table

--- src/config.py
+++ src/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     jwt_secret_key: str
     jwt_algorithm: str
     access_token_expire_minutes: int
+    refresh_token_expire_days: int
 
     model_config = SettingsConfigDict(
         env_file=".env",
