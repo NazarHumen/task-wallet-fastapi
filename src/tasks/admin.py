bab136bc97d171c3f66d6e241053898a2bb310b3
NazarHumen <nazargumen172@gmail.com>
2026-07-09 15:52:36 +0300

    feat(tasks): restrict tasks admin to editing title and description

--- src/tasks/admin.py
+++ src/tasks/admin.py
@@ -1,10 +1,14 @@
 from sqladmin import ModelView
+from wtforms.validators import Length
 
 from src.tasks.models import Task
 
 
 class TaskAdmin(ModelView, model=Task):
-    columns = [
+    can_create = False
+    can_delete = False
+
+    column_list = [
         Task.id,
         Task.title,
         Task.description,
@@ -14,3 +18,9 @@ class TaskAdmin(ModelView, model=Task):
         Task.assignee_id,
         Task.created_at,
     ]
+    column_searchable_list = [Task.description]
+    form_columns = [Task.title, Task.description]
+    form_args = {
+        "title": {"validators": [Length(min=1, max=255)]},
+        "description": {"validators": [Length(max=2000)]},
+    }
