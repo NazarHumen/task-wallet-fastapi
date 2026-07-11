3cba8ea93757bc79f1a9e4449b9b07d55f00a39c
NazarHumen <nazargumen172@gmail.com>
2026-07-11 21:02:48 +0300

    feat(tags): register tags router

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from fastapi.responses import RedirectResponse
 from src.admin_panel import setup_admin
 from src.auth.router import router as auth_router
 from src.db.database import engine
+from src.tags.router import router as tags_router
 from src.tasks.router import router as tasks_router
 
 app = FastAPI(title="TaskWallet")
@@ -19,3 +20,4 @@ def root():
 
 app.include_router(auth_router)
 app.include_router(tasks_router)
+app.include_router(tags_router)
