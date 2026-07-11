85e5ff87c1dff98a0a589149262ce00ec2d8ae12
NazarHumen <nazargumen172@gmail.com>
2026-07-11 21:02:33 +0300

    feat(tasks): manage and search task tags in admin

--- src/tasks/admin.py
+++ src/tasks/admin.py
@@ -13,13 +13,14 @@ class TaskAdmin(ModelView, model=Task):
         Task.title,
         Task.description,
         Task.reward,
+        Task.tags,
         Task.status,
         Task.creator_id,
         Task.assignee_id,
         Task.created_at,
     ]
-    column_searchable_list = [Task.description]
-    form_columns = [Task.title, Task.description]
+    column_searchable_list = [Task.description, "tags.name"]
+    form_columns = [Task.title, Task.description, Task.tags]
     form_args = {
         "title": {"validators": [Length(min=1, max=255)]},
         "description": {"validators": [Length(max=2000)]},
