e7bc36a2a191808ed92130b2bfc0195c0af4f401
NazarHumen <nazargumen172@gmail.com>
2026-07-21 16:48:26 +0300

    feat(payments): register payments router

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from fastapi.responses import RedirectResponse
 from src.admin_panel import setup_admin
 from src.auth.router import router as auth_router
 from src.db.database import engine
+from src.payments.router import router as payments_router
 from src.tags.router import router as tags_router
 from src.tasks.router import router as tasks_router
 from src.transactions.router import router as transactions_router
@@ -23,3 +24,4 @@ app.include_router(auth_router)
 app.include_router(tasks_router)
 app.include_router(tags_router)
 app.include_router(transactions_router)
+app.include_router(payments_router)
